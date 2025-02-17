a6f6f56c744aea1fe7f833d17acc513ea85f7e62
kausik <kkausik11@gmail.com>
2025-02-17 10:13:36 +0530

    prime number

--- Intermediate/23.js
+++ Intermediate/23.js
@@ -1,7 +1,11 @@
 // 11.	Write a JavaScript function that accepts a number as a parameter and checks whether it is prime or not?
 
 function check(num) {
-    let sqareroot = Math.sqrt(num)
+    let sqareroot = Math.sqrt(num);
+    if ( num ===1 || 0) {
+      console.log("not a prime number");
+      return;
+    }
     for (let index = 2; index <= sqareroot; index++) {
        if (num%index == 0) {
           console.log("not a prime number");
@@ -13,6 +17,6 @@ function check(num) {
     
 }
 
-check(11);
+check(2);
 // 12.	How to check if a number is a power of 2?
 // 13.	How to check if a number is a power of 3?
